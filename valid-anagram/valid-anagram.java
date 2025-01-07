d34665ff2f4f622812e07390936effbc5160bf6d
Juhyun Kim <57345435+jukekxm@users.noreply.github.com>
2025-01-08 01:29:40 +0900

    Time: 15 ms, Memory: 44.9 MB - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -5,7 +5,6 @@ class Solution {
         }
         
         Map<Character, Integer> charMap = new HashMap<>();
-        Map<Character, Integer> charMap2 = new HashMap<>();
 
         for (char c : s.toCharArray()) {
             if (charMap.get(c) == null) {
@@ -23,18 +22,12 @@ class Solution {
                 return false;
             }
             
-            if (charMap2.get(c) == null) {
-                charMap2.put(c, 1);
-            }
-            
-            else {
-                int count = charMap2.get(c);
-                charMap2.put(c, count + 1); 
-            }
+            int count = charMap.get(c);
+            charMap.put(c, count - 1);
         }
         
         for (Character c : charMap.keySet()) {
-            if (!charMap.get(c).equals(charMap2.get(c))) {
+            if (charMap.get(c) != 0) {
                 return false;
             }
         }
